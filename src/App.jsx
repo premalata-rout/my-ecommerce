bf910df7acfb9bafa95a4e2bc57b635b2e59b9a6
premalata-rout <premalatarout2004@gmail.com>
2026-09-24 23:30:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 
 const API = "https://node-backend-5hzc.onrender.com";
 
